c8e3f2f2a3f2105ca3073be35e237927f5076223
effloresco <1@mail.com>
2026-05-23 17:55:50 +0300

    feat: change quantity and bought status

--- script.js
+++ script.js
@@ -16,13 +16,37 @@ function render() {
         li.innerHTML = `
             <span class="product-name">${product.name}</span>
             <div class="controls">
-                <span class="count-label">${product.count}</span>
+                ${!product.isBought ? `
+                    <button class="btn-round btn-minus ${product.count === 1 ? 'disabled' : ''}"
+                        ${product.count === 1 ? 'disabled' : ''}>−</button>
+                    <span class="count-label">${product.count}</span>
+                    <button class="btn-round btn-plus">+</button>
+                ` : `<span class="count-label">${product.count}</span>`}
             </div>
             <div class="actions">
                 <button class="btn-status">${product.isBought ? 'Не куплено' : 'Куплено'}</button>
                 ${!product.isBought ? `<button class="btn-remove">×</button>` : ''}
             </div>
         `;
+
+        const plusBtn = li.querySelector('.btn-plus');
+        const minusBtn = li.querySelector('.btn-minus');
+        if (plusBtn) plusBtn.onclick = () => {
+            product.count++;
+            render();
+        };
+
+        if (minusBtn) minusBtn.onclick = () => {
+            if (product.count > 1) {
+                product.count--;
+                render();
+            }
+        };
+
+        li.querySelector('.btn-status').onclick = () => {
+            product.isBought = !product.isBought;
+            render();
+        };
 
         const removeBtn = li.querySelector('.btn-remove');
         if (removeBtn) {
